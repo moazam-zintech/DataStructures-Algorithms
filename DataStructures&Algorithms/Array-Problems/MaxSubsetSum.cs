db8e58dcda0477753525f01fa23999721d6fc92c
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-05-29 20:18:30 +0500

    Day2 of DataStructure and Algorithm

---
+++ DataStructures&Algorithms/Array-Problems/MaxSubsetSum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms.Array_Problems
+{
+    internal class MaxSubsetSum
+    {
+
+        //There are two ways to find subset of string
+        //back tracking and bitwise
+    }
+}
--- DataStructures&Algorithms/Program.cs
+++ DataStructures&Algorithms/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace DataStructures
+﻿using System.Collections.Specialized;
+
+namespace DataStructures
 {
     public class Program
     {
         public static void Main(string[] args) 
         {
-         
+
         }
     }
 }
