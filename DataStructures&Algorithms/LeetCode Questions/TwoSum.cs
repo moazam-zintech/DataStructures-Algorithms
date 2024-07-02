bf3e64ba0307bf606ef13e1b0928340af76c22e0
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-07-02 11:57:46 +0500

    Two Sum Leetcode Question in C#
    https://leetcode.com/problems/two-sum

---
+++ DataStructures&Algorithms/LeetCode Questions/TwoSum.cs
@@ -0,0 +1,26 @@
+﻿namespace DataStructures
+{
+    public static class TwoSumClass
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int temp = i + 1;
+                for (int j = 0; j < (nums.Length - (i + 1)); j++)
+                {
+                    if (nums[i] + nums[temp] == target)
+                    {
+                        result[0] = i;
+                        result[1] = temp;
+                        return result;
+                    }
+                    temp++;
+                }
+            }
+            return result;
+        }
+    }
+}
--- DataStructures&Algorithms/Program.cs
+++ DataStructures&Algorithms/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.ComponentModel.Design;
+﻿
 namespace DataStructures
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            int[] num = { 3,3,7,2,2,4 };
-            Console.WriteLine(ThreeConsectiveNumbers.ThreeConsectiveOdds(num)
-        );
+            int[] num = { 0, 13, 12, 3, 4, 6, 34, 43, 8, 70, 30 };
+            int[] arr= TwoSumClass.TwoSum(num,100);
+            Console.WriteLine("["+arr[0] + "," + arr[1]+"]");
         }
     }
 }
