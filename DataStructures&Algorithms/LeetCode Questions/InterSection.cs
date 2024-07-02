14331d9912fd326f1661e6817664a410e4e1876e
moazam-zintech <muhammad.moazam@zintechnologies.com>
2024-07-02 13:12:45 +0500

    InterSection in C# Leet Code
    https://leetcode.com/problems/intersection-of-two-arrays-ii/

---
+++ DataStructures&Algorithms/LeetCode Questions/InterSection.cs
@@ -0,0 +1,32 @@
+﻿namespace DataStructures
+{
+    public class InterSection
+    {
+        public static int[] InterSection(int[] nums1, int[] nums2)
+        {
+            if (nums2.Length < nums1.Length)
+            {
+                var temp = nums2;
+                nums2 = nums1;
+                nums1 = temp;
+            }
+
+
+            List<int> nums2List, list = new List<int>();
+            nums2List = nums2.ToList();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2List.Count; j++)
+                {
+                    if (nums1[i] == nums2List[j])
+                    {
+                        list.Add(nums1[i]);
+                        nums2List.RemoveAt(j);
+                        break;
+                    }
+                }
+            }
+            return list.ToArray();
+        }
+    }
+}
--- DataStructures&Algorithms/Program.cs
+++ DataStructures&Algorithms/Program.cs
@@ -1,13 +1,11 @@
 ﻿
 namespace DataStructures
 {
-    public class Program
+    public class Program : InterSection
     {
         public static void Main(string[] args)
         {
-            int[] num = { 0, 13, 12, 3, 4, 6, 34, 43, 8, 70, 30 };
-            int[] arr= TwoSumClass.TwoSum(num,100);
-            Console.WriteLine("["+arr[0] + "," + arr[1]+"]");
+
         }
     }
 }
